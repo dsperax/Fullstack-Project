3c89c3631c25e65f80f8f6914d825f76dca08496
Diogo Xavier <diogo.xavier@autoglass.com.br>
2021-08-10 23:22:25 -0300

    v2.0

---
+++ ASP_NET_CORE 2.0/phishing/phishing/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phishing.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public int Pontuacao { get; set; }
+    }
+}
---
+++ ASP_NET_CORE 2.0/phishing/phishing/Services/UsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace phishing.Services
+{
+    public class UsuarioService
+    {
+    }
+}
